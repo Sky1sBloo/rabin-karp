ab39713490305d413cd49e17a4a4b73bfe3e6e3c
Sky1sBloo <ultimate8gaming@gmail.com>
2024-11-25 20:16:03 +0800

    Added spacing on main

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 int main(int argc, char* argv[])
 {
     if (argc != 3) {
-        std::cerr << "Incorrect arguments: " <<
-        "Usage: rabin-karp [text] [pattern]" << std::endl;
+        std::cerr << "Incorrect arguments: " << "Usage: rabin-karp [text] [pattern]" << std::endl;
         return 1;
     }
+
     const std::string text = argv[1];
     const std::string pattern = argv[2];
     RabinKarp patternFinder(283, 113);
+
     const auto& it = patternFinder.getFirstInstanceOf(pattern, text);
     if (it == text.end()) {
         std::cerr << "Cannot find pattern: " << pattern << std::endl;
